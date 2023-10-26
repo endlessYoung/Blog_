7e5e6c05324e702451f17ebe905d2823a6edb701
endlessYoung <1162551456@qq.com>
2023-10-27 00:18:03 +0800

    add ai

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,15 @@
 import Theme from 'vitepress/theme'
 import './style/var.css'
 
+import '@documate/vue/dist/style.css'
+import { h } from 'vue'
+import Documate from '@documate/vue'
+
 export default {
-  ...Theme
+  ...Theme,
+  Layout: h(Theme.Layout, null, {
+    'nav-bar-content-before': () => h(Documate, {
+      endpoint: 'https://izbdbdndfp.us.aircode.run/ask',
+    }),
+  }),
 }
